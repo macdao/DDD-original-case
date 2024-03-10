2ba21cf032a53c4b8732b1ad59dfca1ed813e72c
shiwei.fu <fusw@live.com>
2024-03-10 11:12:17 +0800

    init pbc case

---
+++ pcb/src/test/java/com/logos/ddd/pcb/PcbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logos.ddd.pcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
